b6216919ce86c0d989297bb339597565a0add174
eodyd9684 <eodyd9684@naver.com>
2025-03-24 17:28:54 +0900

    도서등록 api에 첨부파일(도서 이미지) 업로드 기능과 한번에 여러개도서 이미지 등록 INSERT하는 쿼리 작성함

--- src/main/java/com/green/book_shop/book/controller/BookController.java
+++ src/main/java/com/green/book_shop/book/controller/BookController.java
@@ -2,24 +2,32 @@ package com.green.book_shop.book.controller;
 
 import com.green.book_shop.book.dto.BookDTO;
 import com.green.book_shop.book.service.BookService;
+import com.green.book_shop.util.UploadUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/books")
 public class BookController {
   private final BookService bookService;
+  private final UploadUtil uploadUtil;
 
   //도서 등록 api
   //post ~/books
   @PostMapping("")
-  public void insertBook(@RequestBody BookDTO bookDTO){
+  public void insertBook(BookDTO bookDTO
+                        , @RequestParam(name = "mainImg", required = true) MultipartFile mainImg
+                        , @RequestParam(name = "subImg", required = true) MultipartFile subImg
+  ){
     //첨부파일(도서 이미지) 업로드
+    uploadUtil.fileUpload(mainImg);
+    uploadUtil.fileUpload(subImg);
+
     //BOOK 테이블에 데이터 INSERT
     bookService.insertBook(bookDTO);
+
+    //BOOK_IMG 테이블에 도서 이미지 정보 INSERT
   }
 }
